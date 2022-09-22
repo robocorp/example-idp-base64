a08def81ad41191466e6e09701e2d4c8561ed9a4
Tommi Holmgren <tommi.holmgren@gmail.com>
2022-09-22 09:56:14 +0300

    Changed to use latest v of rpaframework with Base64ai keywords

--- convert.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def image_to_base64(filepath):
-    with open(filepath, "rb") as img_file:
-        base64string = base64.b64encode(img_file.read())
-
-    return base64string.decode("utf-8")
